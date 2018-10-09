0c2f730ff5b4c702378647b7ead22d3d9a8a9a6b
Luiggi Tenorio <luiggi.tenorio@pucp.pe>
2018-10-09 17:20:10 -0500

    Working products

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    state = models.IntegerField()
+    description = models.CharField(max_length=300)
+    img_path = models.CharField(max_length=300)
+    #img_path = models.FilePathField(path = '/static/img/',recursive=True)
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.http import HttpResponse
+from .models import Product
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    product_list = Product.objects.order_by('-name')[:5]
+    template = loader.get_template('shop/index.html')
+    context = {
+        'product_list': product_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'shop/index.html')
