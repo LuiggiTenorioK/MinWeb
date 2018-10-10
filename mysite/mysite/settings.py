79c398d165c044807339377c58123068cdf9985f
Fabricio Monsalve <fabricio2111@gmail.com>
2018-10-09 19:04:08 -0500

    allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '8!3_5b=ksvlr!y=td!h^2&d$ko56z-=db-pe54_wkzq!3$*3ng'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
